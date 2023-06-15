50589341d72d0269004e51238f6ac0aa2c8cf76e
ELKZakaria <zakariia.elkhiri@gmail.com>
2023-06-15 13:15:14 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(void){
+/*
+ * main - puts
+ *
+ * Returns: Always 0 (Success)
+ */
+
+int main(){
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - printf
+ *
+ * Returns: Always 0 (Success)
+ */
+
 int main(){
 	printf("with proper grammar, but the outcome is a piece of art,");
 	return 0;
