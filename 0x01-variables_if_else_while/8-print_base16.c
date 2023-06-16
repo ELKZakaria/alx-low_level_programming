f0fd8fe1194c8fdb5ac686e38001b7f3f6b19c62
ELKZakaria <zakariia.elkhiri@gmail.com>
2023-06-16 10:33:53 -0700

    commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints all numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int hex;
+
+	for (hex = 0; hex < 10; hex++)
+	{
+		putchar(hex + '0');
+	}
+	for (hex = 'a'; hex < 'f'; hex++)
+	{
+		putchar(hex);
+	}
+	putchar('\n');
+
+	return (0);
+}
