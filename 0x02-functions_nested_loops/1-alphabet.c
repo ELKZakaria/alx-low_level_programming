e0956a6347422a62b050b90da9c8cef5e466b052
ELKZakaria <zakariia.elkhiri@gmail.com>
2023-06-20 05:22:55 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check code
+ *
+ * Description: Prints the lowercase alphabet using _putchar
+ *
+ * Return: 0 (success)
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+	return (0);
+}
