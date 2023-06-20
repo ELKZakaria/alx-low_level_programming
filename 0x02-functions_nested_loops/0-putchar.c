4761cfa587f31f8072e040f282cd23bba30ea97b
ELKZakaria <zakariia.elkhiri@gmail.com>
2023-06-20 04:56:17 -0700

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *
+ * Description: print '_putchar'
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
