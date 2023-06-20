fbb2809ad4c55cac2a32ea6774645d3a0adc4750
ELKZakaria <zakariia.elkhiri@gmail.com>
2023-06-20 14:15:47 -0700

    commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,20 +1,26 @@
 #include "main.h"
 
+/**
+ * print_sign - Prints the sign of a number
+ * @n: The number to check the sign of
+ *
+ * Return: 1 or 0 or -1
+ */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
-		return 1;
+		_putchar("+");
+		return (1);
 	}
 	else if (n < 0)
 	{
-		printf("-");
-		return -1;
+		_putchar("-");
+		return (-1);
 	}
 	else
 	{
-		printf("0");
-		return 0;
+		_putchar("0");
+		return (0);
 	}
 }
