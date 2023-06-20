31faf1d194d84317e80e938c78481d482ab1ead7
ELKZakaria <zakariia.elkhiri@gmail.com>
2023-06-20 06:00:01 -0700

    commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the lowercase alphabet 10 times
+ *
+ * Description: Prints the lowercase alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		print_alphabet();
+	}
+}
