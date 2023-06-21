96626ebc67e137e99a6b269bd272d59efbd10bb1
ELKZakaria <zakariia.elkhiri@gmail.com>
2023-06-21 09:58:04 -0700

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,7 +25,11 @@ void times_table(void)
 					_putchar(' ');
 				}
 			}
-			_putchar(product + '0');
+			if (product >= 10)
+			{
+				_putchar('0' + product / 10);
+				_putchar('0' + product % 10);
+			}
 		}
 		_putchar('\n');
 	}
