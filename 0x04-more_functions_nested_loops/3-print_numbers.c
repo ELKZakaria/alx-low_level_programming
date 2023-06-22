14dbab6cabf6791b82a3c9a05a399384e69279e8
ELKZakaria <zakariia.elkhiri@gmail.com>
2023-06-22 10:26:17 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print from 0 to 9 *
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i; i < 10; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
