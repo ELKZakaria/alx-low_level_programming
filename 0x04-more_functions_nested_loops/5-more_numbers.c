83cadc40b55c57058286d263ec5778976d30e843
ELKZakaria <zakariia.elkhiri@gmail.com>
2023-06-22 13:14:06 -0700

    commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,13 +13,9 @@ void more_numbers(void)
 		{
 			if (j > 9)
 			{
-				_putchar('0' + j / 10);
-				_putchar('0' + j % 10);
+				_putchar('1');
 			}
-			else
-			{
-				_putchar('0' + j);
-			}
+			_putchar('0' + j % 10);
 		}
 		_putchar('\n');
 	}
