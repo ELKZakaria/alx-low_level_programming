f2e2cd77907d8de3a68a934c1e9e51c18f35fd35
ELKZakaria <zakariia.elkhiri@gmail.com>
2023-06-26 11:41:11 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string.
+ * @s: The input string.
+ *
+ * Return: void.
+ */
+void rev_string(char *s)
+{
+	int len, begin, end;
+
+	len = 0;
+	begin = 0;
+
+	while (s[len] != '\0')
+		len++
+
+	end = len - 1;
+
+	while (start < end)
+	{
+		char tmp;
+
+		tmp = s[begin];
+		s[begin] = s[end];
+		s[end] = tmp;
+
+		begin++;
+		end--;
+	}
+}
