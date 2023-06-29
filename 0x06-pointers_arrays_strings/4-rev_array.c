a2c68616559e6d9d5ee4fddd0aa1401025dec7eb
ELKZakaria <zakariia.elkhiri@gmail.com>
2023-06-29 13:19:09 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse the content
+ * @a: variable int
+ * @n: variable int
+ *
+ * Description: reverse.
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j;
+
+	for (i = 0; i < n--; i++)
+	{
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
+	}
+}
