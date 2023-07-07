7125dc26c2a2db4cfbad3559d74aec837d7c79c5
ELKZakaria <zakariia.elkhiri@gmail.com>
2023-07-07 16:12:23 -0700

    commit

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * Description: description.
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
