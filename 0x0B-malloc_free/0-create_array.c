22f87d83603b16bf47acdeb395fec6d5552386bb
ELKZakaria <zakariia.elkhiri@gmail.com>
2023-07-11 16:18:31 -0700

    commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,16 +10,15 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
 	char *arr;
 
 	if (size == 0)
 		return (NULL);
-	arr = (char *)malloc(size * sizeof(char));
+	arr = malloc(size * sizeof(char));
 
 	if (arr == NULL)
 		return (NULL);
-
-	unsigned int i;
 
 	for (i = 0; i < size; i++)
 	{
