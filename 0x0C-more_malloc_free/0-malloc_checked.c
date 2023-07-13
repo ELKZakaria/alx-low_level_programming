7d99c5d036e67a8667995805ed8c2b023dabc803
ELKZakaria <zakariia.elkhiri@gmail.com>
2023-07-13 08:51:48 -0700

    commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * malloc_checked - allocates memory using malloc
+ * *malloc_checked - allocates memory using malloc
  * @b: unsigned int
  *
- * Description: allocates memory using malloc
  * Return: void
  */
 void *malloc_checked(unsigned int b)
@@ -12,9 +11,8 @@ void *malloc_checked(unsigned int b)
 	void *ptr;
 
 	ptr == malloc(b);
+
 	if (ptr == NULL)
-	{
 		exit(98);
-	}
 	return (ptr);
 }
