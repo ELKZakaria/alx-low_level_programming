cea0218b3de7160a090f27337c4f50f89bbea362
ELKZakaria <zakariia.elkhiri@gmail.com>
2023-07-13 13:02:42 -0700

    commit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,5 +11,21 @@
  */
 int *array_range(int min, int max)
 {
+	int *a, i;
 
+	i = 0;
+	if (min > max)
+		return (NULL);
+	a = malloc((sizeof(int) * (max - min)) + sizeof(int));
+
+	if (a == NULL)
+		return (NULL);
+	while (min <= max)
+	{
+		a[i] = min;
+		i++;
+		min++;
+	}
+
+	return (a);
 }
