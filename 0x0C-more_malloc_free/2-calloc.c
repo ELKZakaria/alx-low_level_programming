39bbfcabb6cd2a22c263042011316f2ec18f25b5
ELKZakaria <zakariia.elkhiri@gmail.com>
2023-07-13 12:57:45 -0700

    commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,5 +11,24 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	int i, j;
+	char *p;
 
+	i = 0;
+	j = 0;
+	if (nmemb == 0 || size == 0)
+		return 	(NULL);
+	j = nmemb * size;
+	p = malloc(j);
+
+	if (p == NULL)
+		return (NULL);
+
+	while (i < j)
+	{
+		p[i] = 0;
+		i++;
+	}
+
+	return (p);
 }
